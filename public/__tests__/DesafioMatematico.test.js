ba5c3fac183c7a1d6d7c44d3d0a356a15972d1cc
garceteclaudio <garcete.claudio@gmail.com>
2024-11-09 20:46:02 -0300

    Se agregaron tests

---
+++ public/__tests__/DesafioMatematico.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import DesafioMatematico from "../../src/pages/DesafioMatematico";
+import "@testing-library/jest-dom";
+
+describe("DesafioMatematico Component", () => {
+  test("Deberia mostrarse correctamente el titulo y la puntuacion inicial.", () => {
+    render(<DesafioMatematico />);
+    expect(screen.getByText(/Desafío Matemático \(1\/5\)/)).toBeInTheDocument();
+    expect(screen.getByText(/Puntaje: 0/)).toBeInTheDocument();
+  });
+
+  test("Deberia terminar el juego despues de 5 desafios.", () => {
+    render(<DesafioMatematico />);
+
+    // Completar los 5 desafios
+    for (let i = 0; i < 5; i++) {
+      const submitButton = screen.getByText("Comprobar");
+      fireEvent.click(submitButton);
+      fireEvent.click(
+        screen.getByText(/Siguiente desafío|Ver resultado final/)
+      );
+    }
+
+    // Check that game-over screen is displayed
+    expect(screen.getByText("Juego Terminado.")).toBeInTheDocument();
+  });
+});
--- src/pages/DesafioMatematico.jsx
+++ src/pages/DesafioMatematico.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import React from 'react';
+
 //import "../stylesheets/DesafioMatematico.css";
 
 function DesafioMatematico() {
