816d5d636538555aa8be155c91024e445cc2772e
garceteclaudio <garcete.claudio@gmail.com>
2024-11-10 19:45:21 -0300

    fix about us

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,53 +1,35 @@
 import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import avatarClaudio from '/resources/images/aboutUs/avatar-clau.png';
+import avatarClaudio from '/resources/images/aboutUs/avatar-clau.jpeg';
 import avatarMurillo from '/resources/images/aboutUs/avatar-murillo.png';
 import avatarYani from '/resources/images/aboutUs/avatar-yani.jpeg';
 
-function AboutUs() {
+function AboutUs({ developers }) {
+  const avatars = {
+    1: avatarClaudio,
+    2: avatarMurillo,
+    3: avatarYani,
+  };
+
   return (
     <Container className="my-5">
       <h1 className="text-center mb-4">GRUPO 6: Desarrolladores de la materia fundamentos de programación web.</h1>
       <Row>
-        <Col md={4}>
-          <Card>
-            <Card.Img variant="top" src={avatarClaudio} alt="Avatar Claudio" />
-            <Card.Body>
-              <Card.Title>Garcete Claudio Rodolfo</Card.Title>
-              <Card.Text>
-                Estudiante TUDIVJ, ubicado en Barrio Malvinas, tengo 2 mascotas,
-                  Tokyo y Bigote. Me gusta hacer musculación y comer en el Lolo
-                  Pulus.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4}>
-          <Card>
-            <Card.Img variant="top" src={avatarMurillo} alt="Avatar Murillo" />
-            <Card.Body>
-              <Card.Title>Murillo Jose Omar</Card.Title>
-              <Card.Text>
-                Actualmente estudiante de TUDIVJ en la UNJU. Generalmente suelo
-                  perder el tiempo en la computadora, aunque a veces lo aprovecho
-                  trabajando.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4}>
-          <Card>
-            <Card.Img variant="top" src={avatarYani} alt="Avatar Yani" />
-            <Card.Body>
-              <Card.Title>Gonzalez Yanina </Card.Title>
-              <Card.Text>
-                Estudiante de la TUDIVJ. Me gusta mucho la música, aparte de
-                estudiar también toco la guitarra y estoy en un grupo de baile.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
+        {developers.map((dev) => (
+          <Col md={4} key={dev.id}>
+            <Card>
+              <Card.Img variant="top" src={avatars[dev.id]} alt={`Avatar ${dev.name}`} />
+              <Card.Body>
+                <Card.Title>{dev.name}</Card.Title>
+                <Card.Text>{dev.description}</Card.Text>
+                <Card.Text>
+                  <a href={dev.repository} target="_blank" rel="noopener noreferrer">GitHub Profile</a>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
